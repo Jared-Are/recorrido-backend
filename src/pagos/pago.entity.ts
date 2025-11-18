ff78dfac1b84e55c10b4218315ed77bd9e54dc5b
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2025-11-18 16:46:43 -0600

    tutor final

--- src/pagos/pago.entity.ts
+++ src/pagos/pago.entity.ts
@@ -1,25 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn } from 'typeorm';
+import { Alumno } from '../alumnos/alumno.entity';
 
-@Entity('pagos') // Crea la tabla 'pagos'
+@Entity('pagos')
 export class Pago {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('uuid') // Más adelante lo uniremos al Alumno
+  @Column('uuid')
   alumnoId: string;
 
   @Column('varchar')
   alumnoNombre: string;
 
-  @Column('float') // Usamos 'float' para dinero
+  @Column('float')
   monto: number;
 
   @Column('varchar')
   mes: string;
 
-  @Column('date', { nullable: true }) // Permite fechas nulas (para 'pendiente')
+  @Column('date', { nullable: true })
   fecha: string; 
 
   @Column('varchar')
   estado: string; // 'pagado' o 'pendiente'
+
+  // --- ESTA ES LA CLAVE QUE FALTABA ---
+  @ManyToOne(() => Alumno)
+  @JoinColumn({ name: 'alumnoId' })
+  alumno: Alumno;
+
+  // Agregamos esto para poder ordenar por fecha de registro si 'fecha' es nula
+  @CreateDateColumn()
+  fechaRegistro: Date;
 }
