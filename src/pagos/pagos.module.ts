80ebf4c6538d58e952892f447bc0a99fe112c2b7
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2025-11-18 17:15:50 -0600

    pruebas tutor v2

--- src/pagos/pagos.module.ts
+++ src/pagos/pagos.module.ts
@@ -3,14 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagosService } from './pagos.service';
 import { PagosController } from './pagos.controller';
 import { Pago } from './pago.entity';
-import { AlumnosModule } from '../alumnos/alumnos.module'; // <-- 1. IMPORTAR
+import { AlumnosModule } from '../alumnos/alumnos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Pago]),
-    AlumnosModule // <-- 2. AÑADIR AQUÍ
-  ], 
+    AlumnosModule
+  ],
   controllers: [PagosController],
   providers: [PagosService],
+  // --- ¡ESTO ES LO QUE FALTA! ---
+  exports: [
+    PagosService, // <-- Exportar el servicio para que TutorModule lo vea
+    TypeOrmModule // Exportar TypeOrm por si acaso
+  ] 
 })
 export class PagosModule {}
