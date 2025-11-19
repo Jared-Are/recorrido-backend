0ad20190262899896b2b15c666078ccac1f2a124
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2025-11-18 21:43:59 -0600

    usuarios v 5

--- src/asistencias/asistencia.module.ts
+++ src/asistencias/asistencia.module.ts
@@ -5,6 +5,10 @@ import { AsistenciaService } from './asistencia.service';
 import { AsistenciaController } from './asistencia.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Asistencia } from './asistencia.entity'; // <-- Solo importa su PROPIA entidad
+import { Alumno } from '../alumnos/alumno.entity';
+import { User } from '../users/user.entity';
+import { Vehiculo } from '../vehiculos/vehiculo.entity';
+import { Aviso } from '../avisos/aviso.entity';
 
 // --- IMPORTA LOS MÓDULOS DE LOS QUE DEPENDES ---
 import { UsersModule } from '../users/users.module';
@@ -14,10 +18,15 @@ import { AvisosModule } from '../avisos/avisos.module';
 import { ConfiguracionModule } from '../configuracion/configuracion.module';
 import { DiasNoLectivosModule } from '../dias-no-lectivos/dias-no-lectivos.module';
 
+
 @Module({
   imports: [
     // 1. Registra SOLAMENTE la entidad 'Asistencia'
-    TypeOrmModule.forFeature([Asistencia]),
+    TypeOrmModule.forFeature([Asistencia, 
+      Alumno, 
+      User, 
+      Vehiculo, 
+      Aviso]),
 
     // 2. Importa los MÓDULOS que proveen las otras entidades
     UsersModule,
