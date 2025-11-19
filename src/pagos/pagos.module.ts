c4d99dbf34306ba48a7764d5249ed6656e3aa92c
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2025-11-18 21:36:32 -0600

    usuarios reparacion v 7

--- src/pagos/pagos.module.ts
+++ src/pagos/pagos.module.ts
@@ -3,16 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagosService } from './pagos.service';
 import { PagosController } from './pagos.controller';
 import { Pago } from './pago.entity';
-// 1. IMPORTA LA ENTIDAD ALUMNO
-import { Alumno } from '../alumnos/alumno.entity'; 
+import { Alumno } from '../alumnos/alumno.entity';
 
 @Module({
   imports: [
-    // 2. AGREGA ALUMNO AQUÍ
-    // Esto le permite al PagosService usar @InjectRepository(Alumno)
-    TypeOrmModule.forFeature([Pago, Alumno]), 
+    TypeOrmModule.forFeature([Pago, Alumno]),
   ],
   controllers: [PagosController],
   providers: [PagosService],
+  // ¡ESTA ES LA CLAVE!
+  // Al exportarlo, permitimos que TutorModule (y otros) lo usen.
+  exports: [PagosService], 
 })
 export class PagosModule {}
